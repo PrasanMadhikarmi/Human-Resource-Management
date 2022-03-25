f3174f545da3c8e9fb291de8b6e436795d8b8bbd
sauravnir <100860107+sauravnir@users.noreply.github.com>
2022-03-25 09:39:03 +0545

    Add files via upload
    New updated code with directories!

--- hrm/main.php
+++ hrm/main.php
@@ -1,4 +1,4 @@
-<!-- <?php include('adminlogin.php') ?> -->
+<!-- <?php include('./PHP/adminlogin.php') ?> -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +6,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="main-style.css" />
+    <link rel="stylesheet" href="./HTML/main-style.css" />
     <title>Login Page</title>
 </head>
 
@@ -25,13 +25,13 @@
                         <input type="password" placeholder="Password"  name="employee_pass"/>
                     </div>
                     <div class="forgot_password">
-                        <a href="./forgotpass.html">Forgot Password?</a>
+                        <a href="./HTML/forgotpass.html">Forgot Password?</a>
                     </div>
                     <input class="btn solid" type="submit" value="Login" />
                 </form>
 
 
-                <form action="adminlogin.php" class="sign-up-form" method="post">
+                <form action="./PHP/adminlogin.php" class="sign-up-form" method="post">
                     <h2 class="title">Admin Login</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
@@ -42,7 +42,7 @@
                         <i class="fas fa-lock"></i>
                         <input type="password" placeholder="Password" name="admin_password" required/>
                     </div>
-                    <input type="submit" class="btn" value="Login" onclicked="forgotpass.html">
+                    <input type="submit" class="btn" value="Login" onclicked="./HTML/forgotpass.html">
                 </form>
 
             </div>
@@ -56,18 +56,18 @@
                     <button class="btn transparent" id="sign-up-btn">Admin Login</button>
                 </div>
 
-                <img src="employee.png" class="image" alt="" />
+                <img src="./assets/employee.png" class="image" alt="" />
             </div>
             <div class="panel right-panel">
                 <div class="content">
                     <!-- <h3>Click below for employee login!</h3> -->
                     <button class="btn transparent" id="sign-in-btn">Employee</button>
                 </div>
-                <img src="admin.png" class="image" alt="" />
+                <img src="./assets/admin.png" class="image" alt="" />
             </div>
         </div>
     </div>
-    <script src="app.js"></script>
+    <script src="./JS/app.js"></script>
 
 </body>
 
